1d7dfda62624f2977d81ba7ded0a289e779ae552
Timos Valais <timosbal@gmail.com>
2023-04-12 23:52:05 +0200

    increased step to avoid falling through things

--- models/contracts/gameObject.js
+++ models/contracts/gameObject.js
@@ -8,7 +8,7 @@ export default class GameOjbect {
     height,
     colorObject = new ColorObject(),
     stepX = 5,
-    stepY = 10
+    stepY = 20
     //shape = null
   ) {
     this.x = x;
@@ -20,21 +20,6 @@ export default class GameOjbect {
     this.stepY = stepY;
   }
   collidesWith(otherObject) {
-    // console.log("collides with : ", otherObject, "this is : ", this);
-    // console.log("collides with : ", otherObject.x, "this is : ", this.x);
-    // console.log("collides with : ", otherObject.y, "this is : ", this.y);
-    // console.log(
-    //   "collides with : ",
-    //   otherObject.width,
-    //   "this is : ",
-    //   this.width
-    // );
-    // console.log(
-    //   "collides with : ",
-    //   otherObject.height,
-    //   "this is : ",
-    //   this.height
-    // );
     //TODO:tv maybe remove these for memory allocation and just do straight the comparisons
     let leftPointBeforeRightPoint = this.x < otherObject.x + otherObject.width;
     let rightPointAfterLeftPoint = this.x + this.width > otherObject.x;
@@ -43,24 +28,18 @@ export default class GameOjbect {
 
     let lowestPointIsBelowHighest = this.y < otherObject.y + otherObject.height;
     let highestPointIsAboveLowest = this.y + this.height > otherObject.y;
-    // console.log(
-    //   "the comparisson : ",
-    //   lowestPointIsBelowHighest,
-    //   highestPointIsAboveLowest
-    // );
+
     let horizontalCollision =
       lowestPointIsBelowHighest && highestPointIsAboveLowest;
     if (verticalCollision) {
       //prioritize vertical from horizontal collitions
-      // console.log("the verticalCollision : ", verticalCollision);
-      // console.log("vertical collided first");
       let over =
         this.y <= otherObject.y + otherObject.height + this.stepY &&
         this.y >= otherObject.y + otherObject.height - this.stepY;
       let under =
         this.y + this.height <= otherObject.y + this.stepY &&
         this.y + this.height >= otherObject.y - this.stepY;
-      // console.log("is it done? : ", over || under);
+      if (over) console.log("returning UP!!!!!");
       if (over) return Directions.UP;
       if (under) return Directions.DOWN;
     }
@@ -68,24 +47,9 @@ export default class GameOjbect {
       let left =
         this.x <= otherObject.x + otherObject.width + this.stepX &&
         this.x >= otherObject.x + otherObject.width - this.stepX;
-      // if (left)
-      //   console.log(
-      //     "left collision : ",
-      //     this.x,
-      //     otherObject.x,
-      //     otherObject.width
-      //   );
       let right =
         this.x + this.width <= otherObject.x + this.stepX &&
         this.x + this.width >= otherObject.x - this.stepX;
-      // console.log("right non collision : ", this.x, this.width, otherObject.x);
-      if (right)
-        console.log(
-          "right collision : ",
-          this.x,
-          otherObject.x,
-          otherObject.width
-        );
       if (left) return Directions.LEFT;
       if (right) return Directions.RIGHT;
     }
@@ -99,6 +63,11 @@ export default class GameOjbect {
       p5Map.groundY - this.y - this.height,
       this.width,
       this.height
+    );
+  }
+  handleCollisions(collisionObjects) {
+    collisionObjects.forEach((collisionObject) =>
+      console.log("Oh no, I collided with ", collisionObject, "object")
     );
   }
 }
