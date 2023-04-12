e10795da9b2d9636accc18d1c4c866672e5f5a7f
Timos Valais <timosbal@gmail.com>
2023-04-12 23:52:20 +0200

    added logic for multiple interactions

--- models/movingObjects/character.js
+++ models/movingObjects/character.js
@@ -5,7 +5,6 @@ export default class Character extends GameOjbect {
   #verticalSpeed = 0;
   #horizontalSpeed = 0;
   #jumps = 0;
-  #froze = false;
   #isGrounded = true;
   constructor(
     x,
@@ -41,12 +40,6 @@ export default class Character extends GameOjbect {
       this.#verticalSpeed -= this.gravity;
     }
 
-    if (this.#froze) {
-      console.log("frozen");
-      console.log("vertical speed : ", this.#verticalSpeed);
-      console.log("gravity", this.gravity);
-    }
-
     // Update the y position with the vertical speed
     this.y += this.#verticalSpeed;
     // If the character touches the ground (y = 0), stop the vertical movement
@@ -59,7 +52,7 @@ export default class Character extends GameOjbect {
       this.#verticalSpeed = 0;
       this.#jumps = 0;
     }
-    if (this.#jumps > 0) {
+    if (!this.#isGrounded) {
       let x1 = this.x;
       let y1 = p5Map.groundY - this.y;
       let x2 = this.x + this.sizeX;
@@ -110,4 +103,50 @@ export default class Character extends GameOjbect {
   start() {
     this.#isGrounded = false;
   }
+  handleCollisions(collisionObjects) {
+    let backgroundObjects = [];
+    let enemies = [];
+    let otherCharacters = [];
+    collisionObjects.forEach((collisionObject) => {
+      switch (collisionObject.type) {
+        case ObjectTypes.BackgroundObject:
+          backgroundObjects.push(collisionObject);
+          break;
+        case ObjectTypes.Enemy:
+          enemies.push(collisionObject);
+          break;
+        case ObjectTypes.Character:
+          otherCharacters.push(collisionObject);
+          break;
+        default:
+          break;
+      }
+      // if (collisionObject.type == ObjectTypes.BackgroundObject) {
+      //   // this.#handleBackgroundObjectCollision(collisionObject);
+      // }
+    });
+    this.#handleBackgroundObjectCollision(backgroundObjects);
+  }
+  #handleBackgroundObjectCollision = (collisionObjects) => {
+    let collision = null;
+    let collisionObject = collisionObjects.find((obj) => {
+      collision = this.collidesWith(obj);
+      return collision != null;
+    });
+
+    switch (collision) {
+      case Directions.UP:
+        this.stop(true, collisionObject.y + collisionObject.height);
+        break;
+      case Directions.DOWN:
+        this.stop(true);
+        break;
+      case Directions.LEFT:
+      case Directions.RIGHT:
+        this.stop();
+      default:
+        this.start();
+        break;
+    }
+  };
 }
